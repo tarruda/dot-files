85fe5c4ec19605b36862be5af62f5ff3df03a067
Thiago de Arruda <tpadilha84@gmail.com>
2012-09-18 22:12:52 -0300

    Updated vim files

--- .zshrc.d/editing.zsh
+++ .zshrc.d/editing.zsh
@@ -1,2 +1,5 @@
 # emacs keybinding
 bindkey -e
+# Enables forward search with ctrl-s, details at:
+# http://aditya.sublucid.com/2009/07/09/best-shell-tip-ever-bck-i-search-or-reverse-i-search-for-the-bash-heads/
+stty stop undef
