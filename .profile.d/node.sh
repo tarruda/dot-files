2d0aa20e7bd13e961cdcc25efb5672ad1a519a36
Thiago de Arruda <tpadilha84@gmail.com>
2012-09-27 16:34:31 -0300

    Changed default npm node version

--- .profile.d/node.sh
+++ .profile.d/node.sh
@@ -1,4 +1,4 @@
 export NVM_DIR="/opt/nvm"
-export NVM_VER="v0.8.8"
+export NVM_VER="v0.8.11"
 export NODE_PATH="$NVM_DIR/$NVM_VER/lib/node_modules"
 [ -d "$NVM_DIR/$NVM_VER/bin" ] && export PATH="$NVM_DIR/$NVM_VER/bin:$PATH"
