a2c90341dc30adcc607aa9a888d78c3a8de4e592
Thiago de Arruda <tpadilha84@gmail.com>
2012-12-26 23:20:05 -0300

    Updated git config

--- .profile.d/node.sh
+++ .profile.d/node.sh
@@ -1,4 +1,4 @@
-export NVM_DIR="/opt/nvm"
-export NVM_VER="v0.8.11"
+export NVM_DIR="$HOME/nvm"
+export NVM_VER="v0.8.16"
 export NODE_PATH="$NVM_DIR/$NVM_VER/lib/node_modules"
 [ -d "$NVM_DIR/$NVM_VER/bin" ] && export PATH="$NVM_DIR/$NVM_VER/bin:$PATH"
