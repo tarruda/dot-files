7a05ddbd30c53aa10bf0189df4c225f05535e37a
Thiago de Arruda <tpadilha84@gmail.com>
2013-01-29 10:01:08 -0300

    Added git-extras as a submodule

--- .profile.d/paths.sh
+++ .profile.d/paths.sh
@@ -1,4 +1,9 @@
-# set PATH so it includes user's private bin if it exists
+# Include git-extras
+if [ -d "$HOME/.git-extras/bin" ] ; then
+    PATH="$HOME/.git-extras/bin:$PATH"
+fi
+
+# Include custom commands
 if [ -d "$HOME/bin" ] ; then
     PATH="$HOME/bin:$PATH"
 fi
--- .zshrc
+++ .zshrc
@@ -2,7 +2,7 @@
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="evan" # afowler, alanpeabody, cypher
 DISABLE_AUTO_UPDATE="true"
-# plugins=(node)
+plugins=(git-extras)
 source $ZSH/oh-my-zsh.sh
 
 # load personal scripts
