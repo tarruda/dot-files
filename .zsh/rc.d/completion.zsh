9ede44f9ad5d6672347004006b8c1a67595c1312
Thiago de Arruda <tpadilha84@gmail.com>
2013-02-13 19:57:15 -0300

    Improved completion config

--- .zsh/rc.d/completion.zsh
+++ .zsh/rc.d/completion.zsh
@@ -5,14 +5,23 @@ setopt complete_in_word
 setopt always_to_end
 
 autoload -U compinit
-compinit -C
+compinit
 # Add local completion functions to fpath:
 local_comp="$ZDOTDIR/completion.d"
 if [ -d $local_comp ]; then
 		fpath=($local_comp $fpath)
+		for fn in $local_comp/*(.N:t); do
+			autoload -U ${fn}
+		done
+		unset fn
 fi
 unset local_comp
 
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format '%B%d%b'
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format 'No matches for: %d'
+zstyle ':completion:*' group-name ''
 zstyle ':completion:*:default' list-colors ''
 zstyle ':completion:*:*:vi:*:*files' ignored-patterns '*.o,*~,*.swp'
 zstyle ':completion:*:*:*:*:*' menu select
