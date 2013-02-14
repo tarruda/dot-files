71e591b1905674948d812ce89cb9cde60a41c06d
Thiago de Arruda <tpadilha84@gmail.com>
2013-02-14 12:15:12 -0300

    Added completion function for the 'em' function

--- .vim/user/ftdetect/zsh.vim
+++ .vim/user/ftdetect/zsh.vim
@@ -1,1 +1,2 @@
 au BufNewFile,BufRead $ZDOTDIR/functions/**/* setl filetype=zsh
+au BufNewFile,BufRead $ZDOTDIR/completion.d/* setl filetype=zsh
---
+++ .zsh/rc.d/compsys.zsh
@@ -0,0 +1,27 @@
+# load directory of custom completion functions
+comp_dir="$ZDOTDIR/completion.d"
+if [ -d $comp_dir ]; then
+	fpath=($comp_dir $fpath)
+fi
+unset comp_dir
+
+# configuration created using compinstall
+zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
+zstyle ':completion:*' expand suffix
+zstyle ':completion:*' file-sort name
+zstyle ':completion:*' format '%d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' insert-unambiguous true
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' list-suffixes true
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=0
+zstyle ':completion:*' original true
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' squeeze-slashes true
+
+# initialize the completion system
+autoload -Uz compinit
+compinit
+
