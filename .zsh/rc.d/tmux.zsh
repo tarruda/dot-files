50ead6a03c7f3376de2a3d19c520441c0746c6c2
Thiago de Arruda <tpadilha84@gmail.com>
2013-02-15 11:32:42 -0300

    Finished fuzzy finder basic implementation

--- .zsh/rc.d/tmux.zsh
+++ .zsh/rc.d/tmux.zsh
@@ -80,74 +80,7 @@ else
 	source "$ZDOTDIR/tmux.d/common.zsh"
 
   vi() {
-    local sid=`tmux display-message -p '#S'`
-    # Get session/window ids
-    # Lets see if we are in a project
-    local dir="`pwd`"
-    local vim_id_pattern=""
-    while [ -n "$dir" ] ; do
-			# Only work with svn 1.7 +
-      [[ -d "$dir/.git" ||\
-        -d "$dir/.svn" ||\
-        -d "$dir/.hg"  ||\
-        -d "$dir/.bzr" ]] && break
-      # go up one level
-      dir=${dir%/*}
-    done
-    # vim servernames are case-insensitive, so we cant use directory
-		# names as parts of the servername(it would generate ambiguities
-		# with names which differ only in case), so each directory/sid 
-		# pair is mapped to a unique id, which will be used as servername
-		#
-		# since the directory->uuid mapping is global(not specific to a
-		# zsh instance) the data will be stored in the shared memory
-		# daemon, which will also synchronize access to the data.
-		local default=`uuidgen`
-		# convert to uppercase since thats how vim display the servers
-		default=${default:u}
-		# append the session id to the key, because vim instances are
-		# session-specific
-		local dir_id=`_shm_get "$dir:$sid" $default`
-    vim_id_pattern=":${dir_id}:"
-    local vim_id=`vim --serverlist | grep -F "$vim_id_pattern"`
-    if [ -z $vim_id ]; then
-      # vim is not running, so start a new instance 
-      #
-      # Before we can send commands to the new instance, it must be fully
-      # started, so we need to syncronize the startup with this function using a
-      # unique named pipe, which will also allow us to get the new pane id
-      local fifo="/tmp/tmux-vim-pane-$RANDOM"
-      while ! mkfifo $fifo > /dev/null 2>&1; do
-        fifo="/tmp/tmux-vim-pane-$RANDOM"
-      done
-      # g:project_dir can be used by vim scripts that need to know the project
-      # root directory
-      tmux split-window -d -p 70 "vim \
-				-c \"cd $dir\" \
-        -c \"let g:project_dir='$dir'\" \
-        -c vim -c ':silent !echo \$TMUX_PANE >> $fifo'\
-				--servername \":$dir_id:\${TMUX_PANE#*\%}\""
-      local pane_id=`cat $fifo`
-      # will only get here after vim has started
-      pane_id=${pane_id#*\%}
-			rm $fifo
-      vim_id=":$dir_id:$pane_id"
-			# now we associate the two panes in shared memory, so we can more easily
-			# break/join them toguether
-			_shm_set "$pane_id" "${TMUX_PANE#*\%}:bottom"
-			_shm_set "${TMUX_PANE#*\%}" "${pane_id}:top"
-    fi
-    # open all files
-    vim --servername "$vim_id" --remote-send "<ESC>"
-    while [ $# -ne 0 ]; do
-			vim --servername "$vim_id" --remote-send ":e ${(q)1:a}<CR>"
-      shift
-    done
-    # extract the unique pane id from vim_id and navigate to it
-		local pane_uid="%${vim_id#\:$dir_id\:}"
-		local window_uid="`tmux display-message -pt \"$pane_uid\" '#{window_id}'`"
-    tmux select-window -t "$window_uid"
-    tmux select-pane -t "$pane_uid"
+		zsh "$ZDOTDIR/tmux.d/vi.zsh" "$@"
   }
 	alias e=vi
 
--- .zsh/tmux.d/fuzzy_edit.zsh
+++ .zsh/tmux.d/fuzzy_edit.zsh
@@ -1,12 +1,28 @@
 #!/usr/bin/env zsh
+# Simple interactive fuzzy file finder, useful for quickly opening files by
+# typing only parts of the name
 
+
+# Action to be executed on the selected filename
+ACTION=$1
+if [ -z $ACTION ]; then
+	# By default, edit the file
+	ACTION="$EDITOR"
+fi
+# Number of errors allowed when doing approximate matching
+ERRORS_ALLOWED=$2
+if [ -z $ERRORS_ALLOWED ]; then
+	ERRORS_ALLOWED=1
+fi
+# Number of result windows
 RESULTS_HEIGHT=$(($LINES - 2))
-# Simple fuzzy file finder, useful for quickly opening files by typing only
-# parts of the name
+export ACTION ERRORS_ALLOWED RESULTS_HEIGHT
 
 run() {
 	setup
 	trap cleanup INT TERM EXIT
+	find "" >&p &
+	FIND_PID=$!
 	getchar
 	while true; do
 		if [ $KEYCODE != 27 ]; then
@@ -33,7 +49,7 @@ setup() {
 }
 
 setup_ipc() {
-	# This pipe is used for reading the currently typed text
+	# This pipe is used for reading the currently typed text/selected file
 	export INPUT_IPC=`mktemp -u`
 	while ! mkfifo -m 600 "$INPUT_IPC" &>/dev/null; do
 		export INPUT_IPC=`mktemp -u`
@@ -75,24 +91,35 @@ setup_ignore_patterns() {
 setup_screen() {
 	zcurses init
 	zcurses addwin header 1 $COLUMNS 0 0
-	zcurses addwin results $RESULTS_HEIGHT $COLUMNS 1 0
+	# put each result in its own window, this makes easy to highlight lines
+	local i=
+	for i in {1..$RESULTS_HEIGHT}; do
+		zcurses addwin "results_$i" 1 $COLUMNS $i 0
+	done
+	# zcurses addwin results $RESULTS_HEIGHT $COLUMNS 1 0
 	zcurses addwin input_box 1 $COLUMNS $(($RESULTS_HEIGHT + 1)) 0
 }
 
 # Runs in a coproc shell and provides a simple protocol for executing
 # and serializing render actions to the terminal. It is also used to
-# read the currently typed text
+# read the currently typed text/selected file
 renderer() {
 	zcurses string header "Fuzzy search: $CWD" 
 	# reset positions
-	zcurses move results 0 0
 	zcurses move input_box 0 0
-	zcurses refresh header results input_box
+	zcurses refresh header input_box
 	exec 4>"$INPUT_IPC"
 	local cmd=
 	local ib_pos=0
-	local res_line=0
+	local selected_line=
+	local sel_pos=
+	local sel_buffer=
+	local sel_char=
+	local res_line=$RESULTS_HEIGHT
 	local buffer=""
+	local win=
+	local line=
+	local i=
 	while read cmd; do
 		case "$cmd" in
 			ECHO*)
@@ -101,62 +128,135 @@ renderer() {
 				zcurses char input_box "$char"
 				ib_pos=$(($ib_pos + 1))
 				buffer="$buffer$char"
-				zcurses clear header
-				zcurses string header "Fuzzy search: $buffer" 
 				;;
-			BACK)
+			BACKSPACE)
 				if [ $ib_pos -gt 0 ]; then
 					# erase character in the input window
 					zcurses move input_box 0 $(($ib_pos - 1))
 					zcurses char input_box " "
 					zcurses move input_box 0 $((--ib_pos))
 					buffer=${buffer[1,-2]}
-					zcurses clear header
-					zcurses string header "Fuzzy search: $buffer" 
 				fi
 				;;
+			SPACE)
+				zcurses char input_box " "
+				ib_pos=$(($ib_pos + 1))
+				buffer="$buffer "
+				;;
+			UP)
+				if [ $selected_line -gt $(($res_line + 1)) ]; then
+					win="results_$selected_line"
+					zcurses bg $win default/default
+					zcurses refresh $win
+					selected_line=$(($selected_line - 1))
+					win="results_$selected_line"
+					zcurses bg $win black/white
+					zcurses refresh $win
+				fi
+				;;
+			DOWN)
+				if [ $selected_line -lt $RESULTS_HEIGHT ]; then
+					win="results_$selected_line"
+					zcurses bg $win default/default
+					zcurses refresh $win
+					selected_line=$(($selected_line + 1))
+					win="results_$selected_line"
+					zcurses bg $win black/white
+					zcurses refresh $win
+				fi
+				;;
+			ENTER)
+				win="results_$selected_line"
+				# save the current cursor position on the selected line/window
+				zcurses position $win sel_pos
+				# query all chars from 0 to the cursor pos
+				sel_buffer=""
+				for i in {0..$(($sel_pos[2] - 1))}; do
+					zcurses move $win 0 $i
+					zcurses querychar $win sel_char
+					sel_buffer="${sel_buffer}${sel_char[1]}"
+				done
+				zcurses move $win 0 $sel_pos[2]
+				print -u 4 "$CWD/$sel_buffer"
+				;;
 			RESULT*)
-				if [ $res_line -lt $RESULTS_HEIGHT ]; then
+				if [ $res_line -ge 1 ]; then
+					win="results_$res_line"
 					# adds line to result window
-					local line=${${cmd#RESULT}#$CWD/}
-					zcurses string results "$line"
-					zcurses move results $((++res_line)) 0
+					line=${${cmd#RESULT}#$CWD/}
+					if [ $res_line -eq $RESULTS_HEIGHT ]; then
+						# Always highlight the first result
+						zcurses bg $win black/white
+						selected_line=$res_line
+					fi
+					zcurses string $win "$line"
+					zcurses refresh $win
+					res_line=$(($res_line - 1))
 				fi
 				;;
 			CLEAR)
 				# clear the results
-				zcurses clear results
-				zcurses move results 0 0
-				res_line=0
+				res_line=$RESULTS_HEIGHT
+				for i in {1..$RESULTS_HEIGHT}; do
+					win="results_$i"
+					zcurses clear $win
+					zcurses bg $win default/default
+					zcurses move $win 0 0
+					zcurses refresh $win
+				done
 				;;
 			GET)
 				print -u 4 "$buffer"
 				continue
 				;;
 		esac
-		zcurses refresh header results input_box
+		# Always keep the cursor focused on the input window
+		zcurses refresh input_box
 	done
 	exec 4>&-
 }
 
 cleanup() {
 	zcurses end
-	exec 4<&-
 	rm -f "$INPUT_IPC"
+	exec 4<&- &> /dev/null
 	exit
 }
 
 # Reads a single char and sets the char keycode
 getchar() {
-	zcurses input input_box CHAR
+	zcurses input input_box CHAR KEY
 	KEYCODE=`printf "%d" "'$CHAR'"`
 }
 
 process_char() {
 	case $KEYCODE in
-		127) echo "BACK" >&p ;;
-		*) echo "ECHO$CHAR" >&p ;;
+		10)
+			local file=
+		 	echo "ENTER" >&p
+			read -u 4 file
+			exec eval "$ACTION \"$file\""
+		 	;;
+		32)
+		 	echo "SPACE" >&p
+			reset_find
+		 	;;
+		127)
+		 	echo "BACKSPACE" >&p
+			reset_find
+		 	;;
+		*)
+			if [ -n "$CHAR" ]; then
+				echo "ECHO$CHAR" >&p
+				reset_find
+			elif [ -n "$KEY" ]; then
+				echo "$KEY" >&p
+			fi
+			;;
 	esac
+}
+
+reset_find() {
 	# if already running, kill the current finder before restarting
 	kill $FIND_PID &> /dev/null
 	if kill -0 $FIND_PID &> /dev/null; then
@@ -192,12 +292,15 @@ get_current_text() {
 	echo -n "$text"
 }
 
-# Delegates the actual finding to the 'walk' function. This runs aynchronously
-# in a background process
+# Delegates the actual finding to the 'walk' function. This runs as a background job
 find() {
 	setopt extendedglob
 	echo "CLEAR"
-	walk "$CWD" "$1" 0
+	if [ -n "$1" ]; then
+		walk "$CWD" "$1" 0
+	else
+		walk "$CWD" "" 0
+	fi
 	sleep 5
 }
 
@@ -207,10 +310,16 @@ walk() {
 	local matches=$3
 	# explicitly set the scope of the '#ia1' flag so it wont apply to
 	# the negated patterns
-	local fpattern="$cwd/(((#ia1)*${pattern}*)${PRUNE_REL_FILES})${PRUNE_FILES}(.N)"
+	local fpattern=
+	if [ -n "$pattern" ]; then
+		fpattern="$cwd/(((#ia$ERRORS_ALLOWED)*${pattern}*)${PRUNE_REL_FILES})${PRUNE_FILES}(.N)"
+	else
+		fpattern="$cwd/(*${PRUNE_REL_FILES})${PRUNE_FILES}(.N)"
+	fi
 	local dpattern="$cwd/(*${PRUNE_REL_DIRS})${PRUNE_DIRS}(/N)"
 	for file in ${~fpattern}; do
 		echo "RESULT$file"
+		# no need to keep searching after the screen is filled with results
 		[ $((++matches)) -ge $RESULTS_HEIGHT ] && sleep 5 && exit
 	done
 	for dir in ${~dpattern}; do
