21aee364917f1963a16fa163dfe6e1a3176485a5
Thiago de Arruda <tpadilha84@gmail.com>
2013-02-25 22:26:00 -0300

    Updated tmux scripts to use the monitor command

--- .zsh/rc.d/tmux.zsh
+++ .zsh/rc.d/tmux.zsh
@@ -23,71 +23,9 @@ else
 		tmux set -u status
   }
 
-	# unregister from the shm server
-	_shm_unregister() {
-    local socket_path="/tmp/tmux-zsh-vim-shm/socket"
-		if zsocket $socket_path > /dev/null 2>&1; then
-      echo "EXIT|||$_shm_client_id" >&$REPLY
-			exec {REPLY}>&-
-		fi
-	}
-
-	# register to the shm server
-	_shm_register() {
-		local REPLY=
-		local tmux_vim_socket_dir="/tmp/tmux-zsh-vim-shm"
-    local socket_path="$tmux_vim_socket_dir/socket"
-		# ensure the shared memory daemon is running:
-		# - try to acquire the daemon lock, if succeeds:
-		#	  - start a subshell
-		#   - replace the subshell with a new, disowned shell
-		#   - the new shell invokes start_shared_memory to finish
-		#     the daemonization process and do the rest of the job
-		if mkdir "$tmux_vim_socket_dir" > /dev/null 2>&1; then
-			(
-			exec zsh "$ZDOTDIR/tmux.d/shm_daemon.zsh" $tmux_vim_socket_dir &!
-			)
-		fi
-		# now try to connect with the daemon, up to 5 times, increasing the poll
-		# interval with each iteration
-		local interval=
-		for i in 1 2 3 4 5; do
-			if zsocket $socket_path > /dev/null 2>&1; then
-				break
-			fi
-			interval=$((i * 0.5))
-			sleep $interval
-		done
-		if [ -z $REPLY ]; then
-			echo "Error registering with the shared memory daemon" >&2
-			local daemon_pid=
-			if [ -r "$tmux_vim_socket_dir/pid" ]; then
-				daemon_pid=`cat "$tmux_vim_socket_dir/pid"`
-			fi
-			if [ -z $daemon_pid ] || ! ps -p $daemon_pid; then
-				local msg=
-				msg=(
-				"The process pointed by the daemon pid was"
-				"not running, will delete the directory"
-				"'$tmux_vim_socket_dir'"
-				)
-				echo "$msg" >&2
-				rm -rf "$tmux_vim_socket_dir"
-			fi
-			return 1
-		fi
-		_shm_client_id=`uuidgen`
-		echo "ENTER|||$_shm_client_id" >&$REPLY
-		exec {REPLY}>&-
-	}
-
-	source "$ZDOTDIR/tmux.d/common.zsh"
-
   vi() {
 		zsh "$ZDOTDIR/tmux.d/vi.zsh" "$@"
   }
-	alias e=vi
 
-	trap _shm_unregister EXIT
-	_shm_register
+	alias e=vi
 fi
--- .zsh/tmux.d/vi.zsh
+++ .zsh/tmux.d/vi.zsh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
-source "$ZDOTDIR/tmux.d/common.zsh"
 
 sid=`tmux display-message -p '#S'`
 # Get session/window ids
@@ -28,35 +27,30 @@ default=`uuidgen`
 default=${default:u}
 # append the session id to the key, because vim instances are
 # session-specific
-dir_id=`_shm_get "$dir:$sid" $default`
+tmux set -q -o "@$dir:$sid" $default
+dir_id=`tmux show -v "@$dir:$sid"`
 vim_id_pattern=":${dir_id}:"
 vim_id=`vim --serverlist | grep -F "$vim_id_pattern"`
 if [ -z $vim_id ]; then
 	# vim is not running, so start a new instance 
 	#
-	# Before we can send commands to the new instance, it must be fully
-	# started, so we need to syncronize the startup with this function using a
-	# unique named pipe, which will also allow us to get the new pane id
-	fifo="/tmp/tmux-vim-pane-$RANDOM"
-	while ! mkfifo $fifo > /dev/null 2>&1; do
-		fifo="/tmp/tmux-vim-pane-$RANDOM"
-	done
 	# g:project_dir can be used by vim scripts that need to know the project
 	# root directory
+	shared="@$RANDOM" # random variable for synchronization
 	tmux split-window -d -p 70 "vim \
 		-c \"cd $dir\" \
 		-c \"let g:project_dir='$dir'\" \
-		-c vim -c ':silent !echo \$TMUX_PANE >> $fifo'\
+		-c ':silent !tmux set -q $shared \$TMUX_PANE'\
+		-c ':silent !tmux monitor -s $shared'\
 		--servername \":$dir_id:\${TMUX_PANE#*\%}\""
-	pane_id=`cat $fifo`
-	# will only get here after vim has started
+	tmux monitor -w $shared
+	pane_id=`tmux show -v $shared`
 	pane_id=${pane_id#*\%}
-	rm $fifo
 	vim_id=":$dir_id:$pane_id"
 	# now we associate the two panes in shared memory, so we can more easily
 	# break/join them toguether
-	_shm_set "$pane_id" "${TMUX_PANE#*\%}:bottom"
-	_shm_set "${TMUX_PANE#*\%}" "${pane_id}:top"
+	tmux set -q "@$pane_id" "${TMUX_PANE#*\%}:bottom"
+	tmux set -q "@${TMUX_PANE#*\%}" "${pane_id}:top"
 fi
 # open all files
 vim --servername "$vim_id" --remote-send "<ESC>"
