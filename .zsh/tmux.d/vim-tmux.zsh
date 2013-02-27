6f7b02e0e53f13f9b4af43bd192d721947842cdc
Thiago de Arruda <tpadilha84@gmail.com>
2013-02-27 14:54:11 -0300

    Fixed synchronization bug on vim-tmux.zsh

--- .zsh/tmux.d/vim-tmux.zsh
+++ .zsh/tmux.d/vim-tmux.zsh
@@ -9,7 +9,7 @@
 # http://sourceforge.net/mailarchive/forum.php?thread_name=CAAq2XdqyKOMj4%2BVFySA9Qbmi0K4jym%2B0sH-z%2BADJT9ijRKzj2w%40mail.gmail.com&forum_name=tmux-users
 
 while (( $# != 0 )); do
-	file=$1
+	file=${1:a}
 	dir=${file:h}
 	while [[ -n $dir ]] ; do
 		# Only work with svn 1.7 +
@@ -20,28 +20,32 @@ while (( $# != 0 )); do
 		# go up one level
 		dir=${dir:h}
 	done
-	tmux monitor -l "@vim-edit:$dir" # to be safe, synchroninze access to the vim pane
-	vim_pane=`tmux show -v "@vim-edit:$dir" 2> /dev/null`
-	if [[ -z $vim_pane ]] || ! tmux display-message -p -t $vim_pane &> /dev/null; then
+	file=${file#$dir/}
+	lock="vim-edit:$dir"
+	tmux monitor -l $lock # to be safe, synchroninze access to the vim pane
+	vim_pane=`tmux show -v "@$lock" 2> /dev/null`
+	if [[ -z $vim_pane ]] || ! tmux display-message -pt $vim_pane &> /dev/null; then
 		# vim is not running in any pane, so start a new instance 
 		#
 		# g:project_dir can be used by vim scripts that need to know the project
 		# root directory
-		monitor="@$RANDOM"
+		monitor="`uuidgen`"
 		tmux split-window -d -p 70 "vim \
 			-c \"cd $dir\" \
 			-c \"let g:project_dir='$dir'\" \
-			-c ':silent !tmux set -q \"@vim-edit:$dir\" \"\$TMUX_PANE\"'\
+			-c ':silent !tmux set -q \"@$lock\" \"\$TMUX_PANE\"'\
 			-c ':silent !tmux monitor -s \"$monitor\"'"
 		tmux monitor -w $monitor
-		vim_pane=`tmux show -v "@vim-edit:$dir" 2> /dev/null`
+		vim_pane=`tmux show -v "@$lock" 2> /dev/null`
 	fi
 	# open all files
-	tmux send-keys -t $vim_pane 'Escape'
-	tmux send-keys -t $vim_pane ":e ${(q)${${file:a}#$dir/}}" "Enter"
+	tmux send-keys -t $vim_pane 'Escape' ":e ${file:q}" 'Enter'
 	window_uid="`tmux display-message -pt \"$vim_pane\" '#{window_id}'`"
-	tmux select-window -t "$window_uid"
-	tmux select-pane -t "$vim_pane"
-	tmux monitor -u "@vim-edit:$dir" &> /dev/null
+	tmux monitor -u $lock
 	shift
 done
+
+if [[ -n $window_uid && -n $vim_pane ]]; then
+	tmux select-window -t $window_uid
+	tmux select-pane -t $vim_pane
+fi
