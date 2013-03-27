ad8a1b162ca626fdfc1ad849730f0ce668310510
Thiago de Arruda <tpadilha84@gmail.com>
2013-03-27 11:58:51 -0300

    Refactored cygwin related files

--- .vimperator/edit-cygwin.bat
+++
@@ -1,4 +0,0 @@
-REM Converts the temp file path to a cygwin path and edit with vim
-set FILE=%~f1
-set FILE=%FILE:\=/%
-D:\LiberKey\MyApps\CygwinPortable\bin\zsh.exe -c "LETTER=${FILE[1]:l};FILE=/cygdrive/${LETTER}${FILE[3,-1]};export FILE;source ~/.zsh/profile.d/00-paths.zsh;source ~/.zsh/profile.d/environment.zsh;DISPLAY=:0.0 urxvt -geometry 100x25 -e vim ${FILE}"
