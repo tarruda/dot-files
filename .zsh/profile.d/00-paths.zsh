db6a9421af1d45b09640e54a6705c168bcab8943
Thiago de Arruda <tpadilha84@gmail.com>
2013-03-27 21:05:59 -0300

    Refactored script that configures PATH

--- .zsh/profile.d/00-paths.zsh
+++ .zsh/profile.d/00-paths.zsh
@@ -1,27 +1,59 @@
-# path for system-wide programs
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin;
+PATH=''
 
-# user binaries
-if [ -d "$HOME/.bin" ]; then
-	PATH="$HOME/.bin:$PATH"
+# path for system programs
+paths=(
+  # default unix paths
+  /sbin
+	/bin
+	/usr/sbin
+	/usr/bin
+	/usr/games
+	/usr/local/sbin
+	/usr/local/bin
+)
+
+if [[ -x /bin/cygpath ]]; then
+	sysroot=$(/bin/cygpath -u $SYSTEMROOT)
+	# windows specific paths
+  paths+=$sysroot	
+	paths+=$sysroot/system32
+	paths+=$sysroot/system32/wbem
+  paths+=$sysroot/system32/windowspowershell/v1.0
+	unset sysroot
 fi
 
-# Include git-extras
-if [ -d "$HOME/.git-extras/bin" ] ; then
-    PATH="$HOME/.git-extras/bin:$PATH"
-fi
+for dir in $paths; do
+ if [[ -d $dir ]]; then
+	 PATH="$PATH:$dir"
+ fi
+done
 
-# path for user installed programs
-if [ -d "$HOME/.user-prefixes" ]; then
-	for prefix in "$HOME/.user-prefixes/"*(/N); do
-		if [ -d "$prefix/bin" ]; then
-			PATH="$prefix/bin:$PATH"
+# path to user programs
+paths=(
+	$HOME/.git-extras/bin
+	$HOME/.bin
+)
+
+for dir in $paths; do
+ if [[ -d $dir ]]; then
+	 PATH="$dir:$PATH"
+ fi
+done
+
+prefixes_dir="$HOME/.user-prefixes"
+
+# paths/manpaths for programs installed in home dir
+if [ -d $prefixes_dir ]; then
+	for dir in $prefixes_dir/*(/N); do
+		if [ -d "$dir/bin" ]; then
+			PATH="$dir/bin:$PATH"
 		fi
-		if [ -d "$prefix/share/man" ]; then
-			MANPATH="$prefix/share/man:$MANPATH"
+		if [ -d "$dir/share/man" ]; then
+			MANPATH="$dir/share/man:$MANPATH"
 		fi
 	done
-	unset prefix
 fi
+
+unset dir paths prefixes_dir
 
 export PATH MANPATH
