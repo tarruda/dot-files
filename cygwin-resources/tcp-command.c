aae69ae2874b31407be3194b82d12ace565e9e48
Thiago de Arruda <tpadilha84@gmail.com>
2013-04-06 01:38:36 -0300

    Added missing files

---
+++ cygwin-resources/tcp-command.c
@@ -0,0 +1,43 @@
+// on a mingw environment, compile with:
+// gcc tcp-command.c -o tcp-command.exe -Wall -DMINGW32 -lws2_32
+#include <winsock2.h>
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+  SOCKET conn;
+  SOCKADDR_IN server_addr;
+  WSADATA wsa_data;
+  WORD version = MAKEWORD(2, 2);
+  char *buffer = (char *)malloc(1024);
+  char *pos = buffer;
+
+  // join the arguments by space into one buffer
+  argc--;
+  argv++;
+  while (argc > 0) {
+    strcpy(pos, *argv);
+    pos += strlen(*argv);
+    strcpy(pos, " ");
+    pos++;
+    argc--;
+    argv++;
+  }
+  if (pos == buffer)
+    return 1; // nothing was provided as argument
+  *(pos - 1) = '\n';
+
+  // open the connection and send the command
+  WSAStartup(version, &wsa_data); 
+  conn = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr("192.168.56.101");
+  server_addr.sin_port = htons(5555);
+  connect(conn, (LPSOCKADDR)&server_addr, sizeof(server_addr));
+  send(conn, buffer, pos - buffer, 0);
+  closesocket(conn);
+  WSACleanup();
+  return 0;
+}
