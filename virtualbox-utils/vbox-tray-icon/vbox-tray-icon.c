930caf59f4e1a54e4460f8c34525519acb8f1816
Thiago de Arruda <tpadilha84@gmail.com>
2013-04-06 21:28:23 -0300

    Refactored vbox-tray-icon

--- virtualbox-utils/vbox-tray-icon/vbox-tray-icon.c
+++ virtualbox-utils/vbox-tray-icon/vbox-tray-icon.c
@@ -6,14 +6,28 @@
 
 #include "vbox-tray-icon.h"
 
-#define WB_TRAY_MSG (WM_USER + 1)
+#define WM_TRAYEVENT (WM_USER + 1)
 #define WB_TRAY_EXIT 1
+
+UINT WM_EXPLORERCRASH = 0 ;
 
 static TCHAR wclass[] = _T("vboxtrayicon");
 static TCHAR title[] = _T("VirtualBox Tray Icon");
 
+NOTIFYICONDATA ndata;
+
 // Tray icon context menu
 HMENU menu;
+
+void run_command() {
+  STARTUPINFO si = { 0 };
+  si.cb = sizeof(si);
+  si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+  si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+  si.hStdOutput =  GetStdHandle(STD_OUTPUT_HANDLE);
+  si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+  si.wShowWindow = SW_HIDE;
+}
 
 void init_menu() {
   menu = CreatePopupMenu();
@@ -38,7 +52,8 @@ LRESULT CALLBACK handle_tray_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM 
       switch (clicked)
       {
         case WB_TRAY_EXIT:
-          result = MessageBox(NULL, "Save the VM state?", "Exit VM instance", MB_YESNO);
+          result = MessageBox(NULL, "Save the VM state?", "Exit VM instance",
+              MB_YESNO);
           if (result == IDYES) PostQuitMessage(0);
           break;
       };
@@ -50,28 +65,29 @@ LRESULT CALLBACK handle_tray_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM 
 
 LRESULT CALLBACK handle_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+  if (msg == WM_EXPLORERCRASH) {
+      Shell_NotifyIcon(NIM_ADD, &ndata);
+      return 0;
+  }
+
   switch (msg)
   {
+    case WM_TRAYEVENT:
+      return handle_tray_message(hWnd, msg, wParam, lParam);
     case WM_CREATE:
       init_menu();
-      break;
-    case WB_TRAY_MSG:
-      return handle_tray_message(hWnd, msg, wParam, lParam);
-      break;
+      return 0;
     default:
       return DefWindowProc(hWnd, msg, wParam, lParam);
   };
-  return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
 {
   WNDCLASSEX wcex;
-  NOTIFYICONDATA ndata;
   MSG msg;
+  HWND hWnd;
 
   // every application that wants to use a message loop needs to
   // initialize/register this structure, as it points to the message handler
@@ -84,7 +100,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
   wcex.hInstance = hInstance;
   wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
   wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.hbrBackground = (HBRUSH)COLOR_APPWORKSPACE;
   wcex.lpszMenuName = NULL;
   wcex.lpszClassName = wclass;
   wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
@@ -92,19 +108,21 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
   // without creating a window no message queue will exist, so this is needed
   // even if the window will be hidden most of the time
-  HWND hWnd = CreateWindow(wclass, title, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, CW_USEDEFAULT, 500, 100, NULL, NULL, hInstance, NULL);
+  hWnd = CreateWindow(wclass, title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+      CW_USEDEFAULT, 500, 100, NULL, NULL, hInstance, NULL);
 
-  // structure containing data about the tray icon
+  // fill structure that holds data about the tray icon
   ndata.cbSize = sizeof(NOTIFYICONDATA);
   ndata.hWnd = hWnd;
-  ndata.uCallbackMessage = WB_TRAY_MSG; // custom message to identify tray events 
+  ndata.uCallbackMessage = WM_TRAYEVENT; // custom message to identify tray events 
   ndata.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_VBOXICON));
   wcscpy(ndata.szTip, "Virtualbox Tray Icon");
   ndata.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
 
-  // add the tray icon
   Shell_NotifyIcon(NIM_ADD, &ndata);
+
+  // listen for the "explorer crash event" so we can add the icon again
+  WM_EXPLORERCRASH = RegisterWindowMessageA("TaskbarCreated");
 
   while (GetMessage(&msg, NULL, 0, 0))
   {
--- virtualbox-utils/vbox-tray-icon/vbox-tray-icon.h
+++ virtualbox-utils/vbox-tray-icon/vbox-tray-icon.h
@@ -1,1 +1,2 @@
 #define IDI_VBOXICON 101
+#define VIRTUALBOX_DIR "C:\\Program Files\\Oracle\\VirtualBox\\"
