13a12a2c2110520324d631d380a734af9576f49e
Thiago de Arruda <tpadilha84@gmail.com>
2013-04-07 00:33:34 -0300

    Implemented vbox-tray-icon basic functions

--- virtualbox-utils/vbox-tray-icon/vbox-tray-icon.c
+++ virtualbox-utils/vbox-tray-icon/vbox-tray-icon.c
@@ -6,32 +6,59 @@
 
 #include "vbox-tray-icon.h"
 
-#define WM_TRAYEVENT (WM_USER + 1)
-#define WB_TRAY_EXIT 1
+#define VBOXDIR "C:\\Program Files\\Oracle\\VirtualBox\\"
+#define STARTVM VBOXDIR "VBoxHeadless.exe --startvm %s --vrdp off"
+#define ACPISHUTDOWN VBOXDIR "VBoxManage.exe controlvm %s acpipowerbutton"
+#define SAVESTATE VBOXDIR "VBoxManage.exe controlvm %s savestate"
 
-UINT WM_EXPLORERCRASH = 0 ;
+#define WM_TRAYEVENT (WM_USER + 1)
+#define WM_TRAY_STARTVM 1
+#define WM_TRAY_ACPISHUTDOWN 2
+#define WM_TRAY_SAVESTATE 3
+#define WM_TRAY_EXIT 10
+
+static UINT WM_EXPLORERCRASH = 0;
 
 static TCHAR wclass[] = _T("vboxtrayicon");
 static TCHAR title[] = _T("VirtualBox Tray Icon");
 
-NOTIFYICONDATA ndata;
+static char format_buffer[200];
+
+static char *vmname;
+
+static NOTIFYICONDATA ndata;
 
 // Tray icon context menu
-HMENU menu;
+static HMENU menu;
 
-void run_command() {
-  STARTUPINFO si = { 0 };
+int run_command(char *cmdline) 
+{
+  PROCESS_INFORMATION pi; 
+  STARTUPINFO si;
+  memset(&si, 0, sizeof(si));
+  memset(&pi, 0, sizeof(pi));
   si.cb = sizeof(si);
   si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
   si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
-  si.hStdOutput =  GetStdHandle(STD_OUTPUT_HANDLE);
+  si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
   si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
   si.wShowWindow = SW_HIDE;
+  return CreateProcess(NULL, cmdline, NULL, NULL, FALSE, CREATE_NO_WINDOW,
+      NULL, NULL, &si, &pi);
+  /* if (vm_pid == 0) { */
+  /*   char buffer[100]; */
+  /*   sprintf(buffer, "error code: %d", GetLastError()); */
+  /*   MessageBox(NULL, buffer, "Debug", MB_OK); */
+  /* } */
 }
 
 void init_menu() {
   menu = CreatePopupMenu();
-  AppendMenu(menu, MF_STRING, WB_TRAY_EXIT, "Exit");
+  AppendMenu(menu, MF_STRING, WM_TRAY_STARTVM, "Start VM");
+  AppendMenu(menu, MF_STRING, WM_TRAY_ACPISHUTDOWN, "ACPI shutdown");
+  AppendMenu(menu, MF_STRING, WM_TRAY_SAVESTATE, "Save VM state");
+  AppendMenu(menu, MF_SEPARATOR, NULL, NULL);
+  AppendMenu(menu, MF_STRING, WM_TRAY_EXIT, "Exit");
 }
 
 LRESULT CALLBACK handle_tray_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -51,8 +78,20 @@ LRESULT CALLBACK handle_tray_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM 
       clicked = TrackPopupMenu(menu, flags, point.x, point.y, 0, hWnd, NULL);
       switch (clicked)
       {
-        case WB_TRAY_EXIT:
-          result = MessageBox(NULL, "Save the VM state?", "Exit VM instance",
+        case WM_TRAY_STARTVM:
+          sprintf(format_buffer, STARTVM, vmname);
+          run_command(format_buffer);
+          break;
+        case WM_TRAY_ACPISHUTDOWN:
+          sprintf(format_buffer, ACPISHUTDOWN, vmname);
+          run_command(format_buffer);
+          break;
+        case WM_TRAY_SAVESTATE:
+          sprintf(format_buffer, SAVESTATE, vmname);
+          run_command(format_buffer);
+          break;
+        case WM_TRAY_EXIT:
+          result = MessageBox(NULL, "Are you sure?", "Exit VM instance",
               MB_YESNO);
           if (result == IDYES) PostQuitMessage(0);
           break;
@@ -66,8 +105,8 @@ LRESULT CALLBACK handle_tray_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM 
 LRESULT CALLBACK handle_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
   if (msg == WM_EXPLORERCRASH) {
-      Shell_NotifyIcon(NIM_ADD, &ndata);
-      return 0;
+    Shell_NotifyIcon(NIM_ADD, &ndata);
+    return 0;
   }
 
   switch (msg)
@@ -82,12 +121,28 @@ LRESULT CALLBACK handle_message(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPara
   };
 }
 
+int parse_options()
+{
+  int argCount;
+
+  if (__argc == 1)
+    return 0;
+
+  vmname = *(__argv + 1);
+  return 1;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpCmdLine, int nCmdShow)
 {
   WNDCLASSEX wcex;
   MSG msg;
   HWND hWnd;
+
+  if (parse_options() == 0) {
+    MessageBox(NULL, "Need to provide the VM name as first argument", "Invalid command line arguments", MB_OK);
+    return 1;
+  }
 
   // every application that wants to use a message loop needs to
   // initialize/register this structure, as it points to the message handler
--- virtualbox-utils/vbox-tray-icon/vbox-tray-icon.h
+++ virtualbox-utils/vbox-tray-icon/vbox-tray-icon.h
@@ -1,2 +1,1 @@
 #define IDI_VBOXICON 101
-#define VIRTUALBOX_DIR "C:\\Program Files\\Oracle\\VirtualBox\\"
