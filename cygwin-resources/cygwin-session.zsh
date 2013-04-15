2ac2b3bdc96586e4b61a1e972c586615880e2ff2
Thiago de Arruda <tpadilha84@gmail.com>
2013-04-15 01:54:48 -0300

    Replace XWin with VcXsrv

--- cygwin-resources/cygwin-session.zsh
+++ cygwin-resources/cygwin-session.zsh
@@ -1,19 +1,22 @@
 #rm -f $HOME/cygwin-session.log
 #exec &> $HOME/cygwin-session.log
 
-{ XWin -wgl -multiwindow -clipboard } &
+cd /cygdrive/d/vcxsrv
+
+{ vcxsrv.exe -wgl -multiwindow -clipboard -ac } &
+# { XWin -wgl -multiwindow -clipboard } &
 
 export DISPLAY=:0.0
 export SHELL=/bin/zsh
 
+cd
+
 { sleep 4 && xrdb $DOTDIR/.Xresources } &
-{ sleep 4 && xhost 192.168.56.50 } &
+# { sleep 4 && xhost 192.168.56.50 } &
 
 if which pulseaudio &> /dev/null; then
 	pulseaudio --start
 fi
-
-cd
 
 nc -k -d -l 127.0.0.1 55555 | while read cmd; do
 	(exec ${~cmd}) &
