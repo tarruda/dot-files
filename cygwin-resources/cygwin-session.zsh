ec90b00953167c4251be30595c1fd6109a0503d9
Thiago de Arruda <tpadilha84@gmail.com>
2013-04-15 01:07:03 -0300

    Improved cygwin environment setup

--- cygwin-resources/cygwin-session.zsh
+++ cygwin-resources/cygwin-session.zsh
@@ -2,9 +2,9 @@
 #exec &> $HOME/cygwin-session.log
 
 { XWin -wgl -multiwindow -clipboard } &
-xwin_pid=$!
 
 export DISPLAY=:0.0
+export SHELL=/bin/zsh
 
 { sleep 4 && xrdb $DOTDIR/.Xresources } &
 { sleep 4 && xhost 192.168.56.50 } &
@@ -16,5 +16,5 @@ fi
 cd
 
 nc -k -d -l 127.0.0.1 55555 | while read cmd; do
-	(exec $cmd) &
+	(exec ${~cmd}) &
 done
