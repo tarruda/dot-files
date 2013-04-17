8b8c598a41a63bcb3f27003e98d31980ef13b24a
Thiago de Arruda <tpadilha84@gmail.com>
2013-04-17 18:11:27 -0300

    Fixed vimperator edit script

--- cygwin-resources/edit-gvim-vimperator.bat
+++ cygwin-resources/edit-gvim-vimperator.bat
@@ -1,3 +1,3 @@
 @echo off
-set FILE=%~f1 && set CYGWIN_X=1 && set DISPLAY=:0.0
-T:\cygwin\bin\zsh.exe -l -c 'gvim -f $(cygpath -u ${FILE})'
+set FILE=%~f1 && set X11_WINDOWS=1 && set DISPLAY=:0.0
+D:\cygwin\bin\zsh.exe -l -c 'gvim -f $(cygpath -u ${FILE})'
--- powershell/register-cygwin-vim.ps1
+++ powershell/register-cygwin-vim.ps1
@@ -5,30 +5,35 @@
 # http://stackoverflow.com/questions/1387769/create-registry-entry-to-associate-file-extension-with-application-in-c
 $ErrorActionPreference = "Stop"
 $classes="hkcu:\software\classes"
-$appid="cygwin.gvim"
-$apps="$classes\applications"
 $cmd='D:\cygwin\home\tarruda\.dotfiles\cygwin-resources\edit-gvim.bat "%1"'
 $friendlyname='gVim (Cygwin)'
 $icon='D:\cygwin\home\tarruda\.dotfiles\cygwin-resources\icons\vim.ico'
-$filetypes=@(".txt", ".ps1", ".sh", ".py", ".cs", ".cpp", ".c", ".rb",
-    ".zsh", ".bash", ".vbox", ".xml", ".yml", ".yaml", ".bat")
 
-if (test-path "$apps\$appid") {
-  # cleanup
-  remove-item -recurse "$apps\$appid"
-}
-# register open commands to know filetypes
-new-item -path "$apps\$appid\shell\open\command" -value $cmd -force -type expandstring
-# add a context menu item(edit with gVim) to every file in windows explorer
+# add 'Edit with gVim' context menu to every file in windows explorer
 new-item -path "$classes\*\shell\Edit with $friendlyname\command" -value $cmd -force -type expandstring
 new-itemproperty -path "$classes\*\shell\Edit with $friendlyname" -name 'Icon' -value $icon -force
-# friendly name for the 'open with' dialog
-new-itemproperty -path "$apps\$appid\shell\open" -name 'FriendlyAppName' -value $friendlyname
-# register the icon
-# FIXME this has no effect, need to find a way to associate icons with a bat file 
-new-item -path "$apps\$appid\DefaultIcon" -value $icon -type expandstring
-# register supported file extensions
-new-item -path "$apps\$appid\SupportedTypes"
-foreach ($ext in $filetypes) {
-  new-itemproperty -path "$apps\$appid\SupportedTypes" -name $ext -PropertyType string -value ''
-}
+
+
+#$apps="$classes\applications"
+#$appid="cygwin.gvim"
+#$filetypes=@(".txt", ".ps1", ".sh", ".py", ".cs", ".cpp", ".c", ".rb",
+#    ".zsh", ".bash", ".vbox", ".xml", ".yml", ".yaml", ".bat")
+#if (test-path "$apps\$appid") {
+#  # cleanup
+#  remove-item -recurse "$apps\$appid"
+#}
+#
+## register open commands to know filetypes
+#new-item -path "$apps\$appid\shell\open\command" -value $cmd -force -type expandstring
+#
+## friendly name for the 'open with' dialog
+#new-itemproperty -path "$apps\$appid\shell\open" -name 'FriendlyAppName' -value $friendlyname
+## register the icon
+## FIXME this has no effect, need to find a way to associate icons with a bat file 
+#new-item -path "$apps\$appid\DefaultIcon" -value $icon -type expandstring
+## register supported file extensions
+#new-item -path "$apps\$appid\SupportedTypes"
+#foreach ($ext in $filetypes) {
+#  new-itemproperty -path "$apps\$appid\SupportedTypes" -name $ext -PropertyType string -value ''
+#}
+#
