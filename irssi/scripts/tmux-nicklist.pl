2362d1415bed858011132ad55fcfea94f90ad6c5
Thiago de Arruda <tpadilha84@gmail.com>
2013-04-25 12:02:13 -0300

    Implemented simple nicklist for tmux/irssi

--- irssi/scripts/tmux-nicklist.pl
+++ irssi/scripts/tmux-nicklist.pl
@@ -1,15 +1,10 @@
+# based on the nicklist.pl script
 use strict;
-use POSIX qw /mkfifo/;
 use IO::Handle;
 use IO::Select;
-use Term::ReadKey;
+use POSIX;
 use File::Temp qw/ :mktemp  /;
-use Data::Dumper;
 use File::Basename;
-
-if ($#ARGV == -1) {
-
-require Irssi;
 use vars qw($VERSION %IRSSI);
 $VERSION = '0.4.6';
 %IRSSI = (
@@ -20,223 +15,158 @@ $VERSION = '0.4.6';
   license     => 'GPLv2',
 );
 
-# temporary directory for this session
-my $tmpdir = mkdtemp "/tmp/nicklist-XXXXXXXX";
-my $fifo_path = "$tmpdir/fifo"; 
+if ($#ARGV == -1) {
+require Irssi;
+
+my $enabled = 0;
 my $script_path = __FILE__;
-my $active_channel;
-my @nicklist;
-# nick => realnick
-# mode =>
-my ($MODE_OP, $MODE_HALFOP, $MODE_VOICE, $MODE_NORMAL) = (0,1,2,3);
-# status =>
-my ($STATUS_NORMAL, $STATUS_JOINING, $STATUS_PARTING, $STATUS_QUITING, $STATUS_KICKED, $STATUS_SPLIT) = (0,1,2,3,4,5);
-# text => text to be printed
-# cmp => text used to compare (sort) nicks
+my $tmpdir;
+my $fifo_path; 
+my $channel_pattern = /^&gtalk$/;
 
-
-# starts the nicklist process in the split pane
-sub start_nicklist {
-  # create the named fifo for this session
-  if (-e $fifo_path) {
-    unlink $fifo_path;
-  }
-  mkfifo($fifo_path, 0600) or die "can\'t mkfifo $fifo_path: $!";
+sub enable_nicklist {
+  return if ($enabled);
+  $tmpdir = mkdtemp "/tmp/nicklist-XXXXXXXX";
+  $fifo_path = "$tmpdir/fifo";
+  POSIX::mkfifo($fifo_path, 0600) or die "can\'t mkfifo $fifo_path: $!";
   my $cmd = "perl $script_path $fifo_path";
   system('tmux', 'split-window', '-dh', '-l', '18', $cmd);
-  # this will block until the other pane has opened the pipe for reading,
-  # so synchronization is not an issue
+  # The next system call will block until the other pane has opened the pipe
+  # for reading, so synchronization is not an issue here.
   open (FIFO, "> $fifo_path") or die "can't open $fifo_path: $!";
   FIFO->autoflush(1);
-  print(FIFO "1\n");
-  sleep 2;
-  print(FIFO "2\n");
-  sleep 2;
-  print(FIFO "3\n");
-  sleep 2;
-  print(FIFO "4\n");
-  sleep 2;
-
+  $enabled = 1;
 }
 
-sub nicklist_write_line {
-  my ($line, $data) = @_;
-  print(FIFO $data);
+sub disable_nicklist {
+  return unless ($enabled);
+  print(FIFO "EXIT\n");
+  close FIFO;
+  unlink $fifo_path;
+  rmdir $tmpdir;
+  $enabled = 0;
 }
 
-# make the (internal) nicklist (@nicklist)
-sub make_nicklist {
-  @nicklist = ();
+sub reset_nicklist {
+  my $active = Irssi::active_win();
+  my $channel = $active->{active};
 
-  ### get & check channel ###
-  my $channel = Irssi->active_win->{active};
-
-  if (!$channel || (ref($channel) ne 'Irssi::Irc::Channel' && ref($channel) ne 'Irssi::Silc::Channel') || $channel->{'type'} ne 'CHANNEL' || ($channel->{chat_type} ne 'SILC' && !$channel->{'names_got'}) ) {
-    $active_channel = undef;
-    # no nicklist
+  if ((!$channel ||
+      (ref($channel) ne 'Irssi::Irc::Channel' && ref($channel) ne
+        'Irssi::Silc::Channel') || $channel->{'type'} ne 'CHANNEL' ||
+      ($channel->{chat_type} ne 'SILC' && !$channel->{'names_got'})) ||
+    ($channel->{'name'} !~ $channel_pattern )) {
+    disable_nicklist;
   } else {
-    $active_channel = $channel;
-    ### make nicklist ###
-    my $thisnick;
+    enable_nicklist;
+    print(FIFO "RESET\n");
     foreach my $nick (sort {(($a->{'op'}?'1':$a->{'halfop'}?'2':$a->{'voice'}?'3':'4').lc($a->{'nick'}))
       cmp (($b->{'op'}?'1':$b->{'halfop'}?'2':$b->{'voice'}?'3':'4').lc($b->{'nick'}))} $channel->nicks()) {
-      $thisnick = {'nick' => $nick->{'nick'}, 'mode' => ($nick->{'op'}?$MODE_OP:$nick->{'halfop'}?$MODE_HALFOP:$nick->{'voice'}?$MODE_VOICE:$MODE_NORMAL)};
-      push @nicklist, $thisnick;
+
+      print(FIFO "APPEND");
+      print(FIFO "$nick->{'nick'};");
+      if ($nick->{'op'}) {
+        print(FIFO "op");
+      } elsif ($nick->{'halfop'}) {
+        print(FIFO "halfop");
+      } elsif ($nick->{'voice'}) {
+        print(FIFO "voice");
+      } else {
+        print(FIFO "normal");
+      }
+      print(FIFO "\n");
     }
+    print(FIFO "ENDPAYLOAD\n");
   }
 }
 
-# insert nick(as hash) into nicklist
-# pre: cmp has to be calculated
-sub insert_nick {
-  my ($nick) = @_;
-  my $nr = find_insert_pos($nick->{'cmp'});
-  splice @nicklist, $nr, 0, $nick;
-  draw_insert_nick_nr($nr);
-}
-
-# remove nick(as nr) from nicklist
-sub remove_nick {
-  my ($nr) = @_;
-  splice @nicklist, $nr, 1;
-}
-
-sub sig_join {
-  my ($server,$channel,$nick,$address) = @_;
-  if (!is_active_channel($server,$channel)) {
-    return;
-  }
-  my $newnick = {'nick' => $nick, 'mode' => $MODE_NORMAL};
-  insert_nick($newnick);
-}
-
-sub sig_kick {
-  my ($server, $channel, $nick, $kicker, $address, $reason) = @_;
-  if (!is_active_channel($server,$channel)) {
-    return;
-  }
-  my $nr = find_nick($nick);
-  if ($nr == -1) {
-    Irssi::print("nicklist warning: $nick was kicked from $channel, but not found in nicklist");
-  } else {
-    remove_nick($nr);
-  }
-}
-
-sub sig_part {
-  my ($server,$channel,$nick,$address, $reason) = @_;
-  if (!is_active_channel($server,$channel)) {
-    return;
-  }
-  my $nr = find_nick($nick);
-  if ($nr == -1) {
-    Irssi::print("nicklist warning: $nick has parted $channel, but was not found in nicklist");
-  } else {
-    remove_nick($nr);
-  }
-
-}
-
-sub sig_quit {
-  my ($server,$nick,$address, $reason) = @_;
-  if ($server->{'tag'} ne $active_channel->{'server'}->{'tag'}) {
-    return;
-  }
-  my $nr = find_nick($nick);
-  if ($nr != -1) {
-    remove_nick($nr);
-  }
-}
-
-sub sig_nick {
-  my ($server, $newnick, $oldnick, $address) = @_;
-  if ($server->{'tag'} ne $active_channel->{'server'}->{'tag'}) {
-    return;
-  }
-  my $nr = find_nick($oldnick);
-  if ($nr != -1) { # if nick was found (nickchange is in current channel)
-    my $nick = $nicklist[$nr];
-    remove_nick($nr);
-    $nick->{'nick'} = $newnick;
-    insert_nick($nick);
-  }
-}
-
-sub sig_mode {
-  my ($channel, $nick, $setby, $mode, $type) = @_; # (nick and channel as rec)
-  if ($channel->{'server'}->{'tag'} ne $active_channel->{'server'}->{'tag'} || $channel->{'name'} ne $active_channel->{'name'}) {
-    return;
-  }
-  my $nr = find_nick($nick->{'nick'});
-  if ($nr == -1) {
-    Irssi::print("nicklist warning: $nick->{'nick'} had mode set on $channel->{'name'}, but was not found in nicklist");
-  } else {
-    my $nicklist_item = $nicklist[$nr];
-    remove_nick($nr);
-    $nicklist_item->{'mode'} = ($nick->{'op'}?$MODE_OP:$nick->{'halfop'}?$MODE_HALFOP:$nick->{'voice'}?$MODE_VOICE:$MODE_NORMAL);
-    insert_nick($nicklist_item);
-  }
-}
-
-##### command binds #####
-# Irssi::command_bind 'nicklist' => sub {
-#   my ( $data, $server, $item ) = @_;
-#   $data =~ s/\s+$//g;
-#   Irssi::command_runsub ('nicklist', $data, $server, $item ) ;
-# };
-# Irssi::signal_add_first 'default command nicklist' => sub {
-#   # gets triggered if called with unknown subcommand
-# };
-
-##### signals #####
-# Irssi::signal_add_last('window item changed', \&make_nicklist);
-# Irssi::signal_add_last('window changed', \&make_nicklist);
-# Irssi::signal_add_last('channel wholist', \&sig_channel_wholist);
-# Irssi::signal_add_first('message join', \&sig_join); # first, to be before ignores
-# Irssi::signal_add_first('message part', \&sig_part);
-# Irssi::signal_add_first('message kick', \&sig_kick);
-# Irssi::signal_add_first('message quit', \&sig_quit);
-# Irssi::signal_add_first('message nick', \&sig_nick);
-# Irssi::signal_add_first('message own_nick', \&sig_nick);
-# Irssi::signal_add_first('nick mode changed', \&sig_mode);
-
-start_nicklist;
-# open_fifo;
+Irssi::signal_add_last('window item changed', \&reset_nicklist);
+Irssi::signal_add_last('window changed', \&reset_nicklist);
+Irssi::signal_add_last('channel wholist', \&reset_nicklist);
+# first, to be before ignores
+Irssi::signal_add_first('message join', \&reset_nicklist);
+Irssi::signal_add_first('message part', \&reset_nicklist);
+Irssi::signal_add_first('message kick', \&reset_nicklist);
+Irssi::signal_add_first('message quit', \&reset_nicklist);
+Irssi::signal_add_first('message nick', \&reset_nicklist);
+Irssi::signal_add_first('message own_nick', \&reset_nicklist);
+Irssi::signal_add_first('nick mode changed', \&reset_nicklist);
+Irssi::signal_add('gui exit', \&disable_nicklist);
 
 } else {
 
 my $fifo_path = $ARGV[0];
-open (FIFO, "< $fifo_path") or die "can't open $fifo_path: $!";
+# array to store the current channel nicknames
+my @nicknames = ();
 
+# helper functions for manipulating the terminal
+# escape sequences taken from
+# http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
+sub clear_screen { print "\e[2J"; }
+sub save_cursor { print "\e[s"; }
+sub restore_cursor { print "\e[u"; }
+sub enable_mouse { print "\e[?1000h"; }
+sub disable_mouse { print "\e[?1000l"; }
+
+# setup terminal so we can listen for individual key presses without echo
+my ($term, $oterm, $echo, $noecho, $fd_stdin);
+$fd_stdin = fileno(STDIN);
+$term = POSIX::Termios->new();
+$term->getattr($fd_stdin);
+$oterm = $term->getlflag();
+$echo = ECHO | ECHOK | ICANON;
+$noecho = $oterm & ~$echo;
+$term->setlflag($noecho);
+$term->setcc(VTIME, 1);
+$term->setattr($fd_stdin, TCSANOW);
+
+# open named pipe and setup the 'select' wrapper object for listening on both
+# fds(fifo and sdtin)
+open (FIFO, "< $fifo_path") or die "can't open $fifo_path: $!";
 my $fifo = \*FIFO;
 my $stdin = \*STDIN;
-
 my $select = IO::Select->new();
+my @ready;
 $select->add($fifo);
 $select->add($stdin);
 
-ReadMode 4;
-
-my @ready;
-
-LOOP: {
+save_cursor;
+enable_mouse; # also disables tmux scrolling for the pane
+MAIN: {
   while (@ready = $select->can_read) {
     foreach my $fd (@ready) {
       if ($fd == $fifo) {
-        my $line = ReadLine 0, $fifo;
-        unless ($line) {
-          last LOOP;
+        while (<$fifo>) {
+          my $line = $_;
+          if ($line =~ /^RESET/) {
+            restore_cursor;
+            clear_screen;
+          } elsif ($line =~ /^APPEND(.+)$/) {
+            my ($nick, $mode) = split(/;/, $1);
+            if ($mode =~ 'op') {
+              print "\e[32m\@$nick\e[39m\n";
+            } elsif ($mode =~ 'halfop') {
+              print "\e[34m%$nick\e[39m\n";
+            } elsif ($mode =~ 'voice') {
+              print "\e[33m+$nick\e[39m\n";
+            } else {
+              print " $nick\n"; 
+            }
+          } elsif ($line =~ /^ENDPAYLOAD$/) {
+            last; 
+          } elsif ($line =~ /^EXIT$/) {
+            last MAIN;
+          }
         }
-        print $line;
       } else {
-        my $key = ReadKey(-1);
-        print "Get key $key\n";
+        my $key = '';
+        sysread(STDIN, $key, 1);
+        # TODO handle mouse scroll
       }
     }
   }
 }
 
-ReadMode 0;
 close FIFO;
 
 }
